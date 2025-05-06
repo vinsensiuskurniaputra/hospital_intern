d4b283c12edfcbea9dee3c893b2235ff254ab5d1
atina-21 <atinanurf21@gmail.com>
2025-05-06 09:04:01 +0700

    enhance menu index.blade.php

--- resources/views/pages/admin/menu/index.blade.php
+++ resources/views/pages/admin/menu/index.blade.php
@@ -22,11 +22,6 @@
                         <div class="flex flex-col lg:flex-row w-full justify-between gap-3">
                             <!-- Search -->
                             <div class="w-full lg:w-[360px]">
-                                <div class="relative">
-                                    <input type="text" id="searchInput" placeholder="Search role..."
-                                        class="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-200 focus:border-[#637F26] focus:ring-2 focus:ring-[#637F26]">
-                                    <i class="bi bi-search absolute left-3 top-2.5 text-gray-400"></i>
-                                </div>
                             </div>
                             <button @click="addModal = true"
                                 class="px-4 py-2 text-sm font-medium text-white bg-[#637F26] rounded-lg hover:bg-[#85A832]">
@@ -45,8 +40,6 @@
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Url</th>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Icon</th>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Parent</th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Order</th>
-                                <th class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase">Actions</th>
                             </tr>
                         </thead>
                         <tbody id="TableBody" class="divide-y divide-gray-100">
@@ -56,11 +49,6 @@
                         </tbody>
                     </table>
                 </div>
-
-                <!-- Pagination -->
-                @include('components.general.pagination', [
-                    'datas' => $menus,
-                ])
 
             </div>
         </div>
